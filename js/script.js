15d7f0859021dc0f79dc5f82abf4f36b9c05422e
BaxtiyorXusanov <xusanovbaxtiyor07>
2021-10-22 19:12:39 +0500

    index page slider end

--- js/script.js
+++ js/script.js
@@ -36,3 +36,36 @@ priceExtraBtn.addEventListener('click', ()=>{
         priceProdBtn.classList.remove('price__title--btn--active')
     }
 })
+
+//******* Slider *********/
+
+let mySlider = new Swiper(".team__slider", {
+    
+    navigation: {
+        nextEl: ".slider_next_btn",
+        prevEl: ".slider_prev_btn",
+    },
+    
+    slidesPerView: 4,
+    spaceBetween: 0,
+
+    //REsponsive
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            // spaceBetween: 0,
+        },
+        560: {
+            slidesPerView: 2,
+            //   spaceBetween: 0,
+        },
+        1115: {
+          slidesPerView: 3,
+        //   spaceBetween: 20,
+        },
+        1427: {
+          slidesPerView: 4,
+        //   spaceBetween: 50,
+        }
+    }
+});
